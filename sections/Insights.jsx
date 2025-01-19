f8ba27c308261a359302c2be2ad805cb883832da
nlphucthnh1303 <nlphucthnh@gmail.com>
2025-01-19 16:38:44 +0700

    pass 90%

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,9 +1,29 @@
 'use client';
 
+import {motion} from 'framer-motion';
+import styles from '../styles';
+import {fadeIn, navVariants, slideIn, staggerContainer, textVariant} from '../utils/motion';
+import {ExploreCard, InsightCard, TitleText, TypingText} from '../components';
+import {exploreWorlds, insights} from "../constants";
+
 const Insights = () => (
-  <section>
-    Insights section
-  </section>
+    <section className={`${styles.paddings} relative z-10`}>
+        <motion.div
+            variants={staggerContainer}
+            initial="hidden"
+            whileInView="show"
+            viewport={{once: 'false', amount: 0.25}}
+            className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col`}>
+
+            <TypingText title="| Insights" textStyles="text-center" />
+            <TitleText title="Insights about metaverse" textStyles="text-center" />
+            <div className="mt-[50px] flex flex-col gap-[30px]">
+                {insights.map((insight, index) => (
+                    <InsightCard />
+                ))}
+            </div>
+        </motion.div>
+    </section>
 );
 
 export default Insights;
